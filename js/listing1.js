8d880f180e8f3d26e6eac8f32abb99376267d77a
Kerry Wargo <kewargo@verizon.net>
2013-11-01 17:11:32 -0400

    New homework repository push to GitHub

---
+++ js/listing1.js
@@ -0,0 +1,28 @@
+// jQuery Document Ready function with task list code within it 
+// to be executed once the HTML has finished loading
+
+$(document).ready(function(){
+
+var tasks = [{'description' : 'mess up my to do app'},
+{'taskDesc':'Change oil in the Ram 2500','completed':false}, 
+{'taskDesc':'Change Oil in Subaru Forrester','completed':false},
+{'taskDesc':'Change Oil in Mini Cooper','completed':true},
+{'taskDesc':'Change air filter in HVAC Air Handler','completed':true},
+{'taskDesc':'Change batteries in Carbon Dioxide Detectors','completed':false}, 
+{'taskDesc':'Bleed air out of hot water radiators','completed':true}, 
+{'taskDesc':'Change water filter in refrigerator','completed':false}];
+
+
+    for(var i=0; i < tasks.length; i++) {
+    
+//        createElement ('<li class="list-group-item success">' + '<span class="glyphicon glyphicon-ok"></span>   ' + task.taskDesc + '</li>');
+    
+    } // End of "For" loop
+
+}); //End of Document Ready Function
+
+// Function to delete completed tasks by selecting tasks with a class of .success
+
+function removeCompleted() {
+    $('.success').remove();   
+}
---
+++ js/listing_save.js
@@ -0,0 +1,45 @@
+js
+var tasks = [{'taskDesc':'Change oil in the Ram 2500','completed':false}, 
+{'taskDesc':'Change Oil in Subaru Forrester','completed':false},
+{'taskDesc':'Change Oil in Mini Cooper','completed':true},
+{'taskDesc':'Change air filter in HVAC Air Handler','completed':true},
+{'taskDesc':'Change batteries in Carbon Dioxide Detectors','completed':false}, 
+{'taskDesc':'Bleed air out of hot water radiators','completed':true}, 
+{'taskDesc':'Change water filter in refrigerator','completed':false}];
+
+document.write('<ul class="list-group">');
+
+ for(var i=0; i < tasks.length; i++) {
+
+/* try-catch logic for error handling, otherwise print tasks */
+
+    try {
+        printTask(tasks[i]);
+        } catch(error) {
+            console.log("There was an error: " + error);
+        }
+    }    
+
+ 
+document.write('</ul>');
+
+
+/* function to print tasks */
+
+function printTask(task) {
+    
+if(typeof task.taskDesc == 'undefined') {
+        throw new Error('This task is missing a description!');
+    }
+    
+    if( typeof task.completed == 'undefined' ) {
+        throw 'This task is missing a completion status!';
+        
+    }
+    
+        if(task.completed) {
+            document.write('<li class="list-group-item success">' + '<span class="glyphicon glyphicon-ok"></span>   ' + task.taskDesc + '</li>'); 
+          } else {
+            document.write('<li class="list-group-item">' + '<span class="glyphicon glyphicon-pushpin"></span>   ' + task.taskDesc + '</li>'); 
+        }
+    }
