6d68167d5c03a20f55d252237b6a3b99c03e0ac8
Kerry Wargo <kewargo@verizon.net>
2013-11-12 09:12:16 -0500

    Completed Homework #4

--- js/listing1.js
+++ js/listing1.js
@@ -1,11 +1,82 @@
 
-// jQuery Document Ready function with task list code within it 
-// to be executed once the HTML has finished loading
+// Global definition of variable tasks
+var tasks;
+
+/* ****************************************************************** */
+/* Create all functions here. jQuery Document Ready function is below */
+/* ****************************************************************** */
+
+// Function to delete completed tasks by selecting tasks with a class of .success
+
+function removeCompleted(){
+    $('.success').remove();   
+}
+
+
+// Function to write tasks to screen
+
+function displayTasks(task) {
+    
+    if(typeof task.taskDesc == 'undefined') {
+        throw 'This task has no task description!';
+    }
+    
+    if(typeof task.completed == 'undefined') {
+        throw 'This task has no completed status!';
+    }
+        
+    if(task.completed) {
+        return $('<li/>', {'class' : 'list-group-item success'}).text(' ' + task.taskDesc).prepend($('<span/>', {'class' : 'glyphicon glyphicon-ok'})).append($('<span/>', {'class' : 'list-group-item-right success'}).text(' ' + task.dueDate));
+    } else {
+        return $('<li/>', {'class' : 'list-group-item'}).text(' ' + task.taskDesc).prepend($('<span/>', {'class' : 'glyphicon glyphicon-pushpin'})).append($('<span/>', {'class' : 'list-group-item-right'}).text(' ' + task.dueDate));
+    }
+}
+
+
+// Constructor function called Task.
+// It returns an array containing taskDesc, completed and dueDate.
+
+function Task(taskDesc) {
+    var arrayObject = {};
+    
+        arrayObject.taskDesc = taskDesc || 'Default task description';
+        arrayObject.completed = false;
+        arrayObject.dueDate = oneWeekFromNow;
+    
+    return arrayObject;  
+    
+}
+
+
+// Calculate date one week from now
+
+var oneWeekFromNow = new Date();
+                oneWeekFromNow.setDate(oneWeekFromNow.getDate() + 7);
+
+
+/* Write a function (addTask) that calls “new Task” and adds the object returned
+from that function to the array where we keep the rest of our task objects. 
+When that’s done, you’ll need to re-render the list HTML. */
+
+function addTask(arrayObject) { new Task();
+    
+    var newTask = new Task(arrayObject);
+    tasks.push(newTask);
+    
+    var li = displayTasks(newTask);
+    
+    $('ul.nav-list').append(li);
+}
+
+/* *************************************************************** */
+/* jQuery Document Ready function with task list code within it    */ 
+/* to be executed once the HTML has finished loading               */
+/* *************************************************************** */
 
 $(document).ready(function (){
 
 var tasks = [{'completed' : false},
-{'description' : 'mess up my to do app'},
+{'taskDesc' : 'mess up my to do app'},
 {'taskDesc' : 'Change oil in the Ram 2500','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}, 
 {'taskDesc' : 'Change Oil in Subaru Forrester','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
 {'taskDesc' : 'Change Oil in Mini Cooper','completed':true, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
@@ -26,68 +97,8 @@ var tasks = [{'completed' : false},
     
     } // End of "For" loop
 
-    $('.panel-primary').append(ul);    
-
+    $('.panel-primary').append(ul);  
+    
 }); //End of Document Ready Function
-
-
-
-// Function to delete completed tasks by selecting tasks with a class of .success
-
-function removeCompleted(){
-    $('.success').remove();   
-}
-
-
-
-// Function to write tasks to screen
-
-function displayTasks(task) {
-    
-    if(typeof task.taskDesc == 'undefined') {
-        throw 'This task has no task description!';
-    }
-    
-    if(typeof task.completed == 'undefined') {
-        throw 'This task has no completed status!';
-    }
-        
-    if(task.completed) {
-        return $('<li/>', {'class' : 'list-group-item success'}).text(' ' + task.taskDesc + ' ' + task.dueDate).prepend($('<span/>', {'class' : 'glyphicon glyphicon-ok'}));
-    } else {
-        return $('<li/>', {'class' : 'list-group-item'}).text(' ' + task.taskDesc + ' ' + task.taskDesc).prepend($('<span/>', {'class' : 'glyphicon glyphicon-pushpin'}));
-    }
-}
-
-
-
-// Constructor function called Task.
-// It returns an array containing name, completed and due date.
-
-function Task(newTask) {
-    this.name = name || "Clean the gas fireplace inserts";
-    this.completed = false;
-    this.dueDate = oneWeekFromNow;
-    
-    return [this.name, this.completed, this.dueDate];  
-    
-}
-
-
-
-// Calculate date one week from now
-
-var oneWeekFromNow = new Date();
-                oneWeekFromNow.setDate(oneWeekFromNow.getDate() + 7);
-
-
-
-/* Write a function (addTask) that calls “new Task” and adds the object returned
-from that function to the array where we keep the rest of our task objects. 
-When that’s done, you’ll need to re-render the list HTML. */
-
-// var addTask = new Task(tasks.push);
-
-
 
 
