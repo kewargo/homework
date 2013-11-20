e1a8b619ee8ed00fd136505058cdbbea0c254428
Kerry Wargo <kewargo@verizon.net>
2013-11-19 19:37:57 -0500

    First pass at Homework #5

--- js/listing5.js
+++ js/listing5.js
@@ -1,6 +1,13 @@
 // Global definition of variable tasks
 var tasks;
 
+var client_creds = {
+        orgName:'kewargo',
+        appName:'maintenancetasks'
+    };
+
+
+
 /* ****************************************************************** */
 /* Create all functions here. jQuery Document Ready function is below */
 /* ****************************************************************** */
@@ -58,12 +65,23 @@ When that’s done, you’ll need to re-render the list HTML. */
 
     function addTask(taskDesc) {
         var newTask = new Task(taskDesc);
-       tasks.push(newTask);
+        
+        console.log(JSON.stringify(newTask));
+
+        $.ajax({
+            'url': 'http://api.usergrid.com/kewargo/maintenancetasks',
+            'type': 'POST',
+            'data' : JSON.stringify(newTask),
+            'success': function(data) {
+                console.log(data);
+                }
+            });
+    
+        tasks.push(newTask);
         var li = displayTasks(newTask);
         $('ul.nav-list').append(li);
+
     }
-
-
 
 /* *************************************************************** */
 /* jQuery Document Ready function with task list code within it    */ 
@@ -71,31 +89,50 @@ When that’s done, you’ll need to re-render the list HTML. */
 /* *************************************************************** */
 
 $(document).ready(function (){
+    
+    //Initializes the SDK. Also instantiates Apigee.MonitoringClient
+var dataClient = new Apigee.Client(client_creds); 
 
-tasks = [{'completed' : false},
-{'taskDesc' : 'mess up my to do app'},
-{'taskDesc' : 'Change oil in the Ram 2500','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}, 
-{'taskDesc' : 'Change Oil in Subaru Forrester','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
-{'taskDesc' : 'Change Oil in Mini Cooper','completed':true, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
-{'taskDesc' : 'Change air filter in HVAC Air Handler','completed':true, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
-{'taskDesc' : 'Change batteries in Carbon Dioxide Detectors','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}, 
-{'taskDesc' : 'Bleed air out of hot water radiators','completed':true, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}, 
-{'taskDesc' : 'Change water filter in refrigerator','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}];
+    $.ajax({
+        'url':'https://api.usergrid.com/kewargo/maintenancetasks',
+        'type':'GET', 'success':function(data) {
+          console.log(data);
+          tasks = data.entities;
+    /*
+    tasks = [{'completed' : false},
+    {'taskDesc' : 'mess up my to do app'},
+    {'taskDesc' : 'Change oil in the Ram 2500','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}, 
+    {'taskDesc' : 'Change Oil in Subaru Forrester','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
+    {'taskDesc' : 'Change Oil in Mini Cooper','completed':true, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
+    {'taskDesc' : 'Change air filter in HVAC Air Handler','completed':true, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'},
+    {'taskDesc' : 'Change batteries in Carbon Dioxide Detectors','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}, 
+    {'taskDesc' : 'Bleed air out of hot water radiators','completed':true, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}, 
+    {'taskDesc' : 'Change water filter in refrigerator','completed':false, 'dueDate' : 'Tue Nov 12 2013 18:32:49 GMT-0500 (EST)'}];
+    */
+        var ul = $('<ul/>', {'class' : 'nav nav-list'});
+        
+            for(var i=0; i < tasks.length; i++) {
+        
+                try {
+                    ul.append(displayTasks(tasks[i]));
+                } catch(error) {
+                    console.log("An error occurred: " + error);
+                }
+        
+            } // End of "For" loop
+    
+            $('.panel-primary').append(ul);
+        }    
+        
+    }); // end of ajax function
 
-    var ul = $('<ul/>', {'class' : 'nav nav-list'});
-    
-    for(var i=0; i < tasks.length; i++) {
-    
-        try {
-            ul.append(displayTasks(tasks[i]));
-        } catch(error) {
-            console.log("An error occurred: " + error);
-        }
-    
-    } // End of "For" loop
 
-    $('.panel-primary').append(ul);  
-    
+    $('#newTaskForm').on('submit', function(event){
+        event.preventDefault();
+
+        var newTaskName = $(this).find('input').val();
+        
+        addTask(newTaskName);
+    });
+
 }); //End of Document Ready Function
-
-
